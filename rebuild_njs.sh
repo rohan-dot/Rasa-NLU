352f89873e50184c5d1c1bfae402d6f2812d141a
rohan-dot <31583020+rohan-dot@users.noreply.github.com>
2026-05-04 11:35:38 -0400

    Delete rebuild_njs.sh

--- rebuild_njs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd secbench_tasks/njs.cve-2022-32414/repo
-make clean
-NJS_CFLAGS="-fsanitize=address -fno-omit-frame-pointer" ./configure
-make -j4
-cd ../../..
-echo "Done. Now run:"
-echo "ASAN_OPTIONS=detect_leaks=0 ./secbench_tasks/njs.cve-2022-32414/repo/build/njs poc_njs.js"
